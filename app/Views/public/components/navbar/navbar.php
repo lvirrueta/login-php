710b99a636d51786c592936b4b14a10b360ba6a5
lvirrueta <luis510vg@gmail.com>
2023-10-04 02:19:31 -0600

    switch button-login on login-page

--- app/Views/public/components/navbar/navbar.php
+++ app/Views/public/components/navbar/navbar.php
@@ -1,6 +1,32 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="<?php echo base_url('/') ?>">App</a>
-    <a class="btn btn-outline-success" href="<?php echo base_url('/login') ?>">Login</a>
+
+    <!-- Back Button -->
+    <div id="backButton">
+      <a class="btn btn-outline-danger" href="<?php echo base_url('/') ?>">Regresar</a>
+    </div>
+    <!-- END Back Button -->
+
+
+    <!-- Login Button -->
+    <div id="loginButton">
+      <a class="btn btn-outline-success" href="<?php echo base_url('/login') ?>">Login</a>
+    </div>
+    <!-- END Login Button -->
+
+
   </div>
 </nav>
+
+<script>
+  const backButton = document.getElementById('backButton');
+  const loginButton = document.getElementById('loginButton');
+  const url = window.location.href;
+
+  if( url.includes('login') ) {
+    loginButton.remove();
+  } else {
+    backButton.remove();
+  }
+</script>
